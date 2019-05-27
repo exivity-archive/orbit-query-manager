0e95e814d12aab49f41890fbb52ff093bbcae3c2
stefanwullems <stefan@exivity.com>
2019-05-27 21:31:48 +0200

    separated out cacheManager

---
+++ src/CacheManager.ts
@@ -0,0 +1,68 @@
+import { Observable } from './Observable'
+import { Data, Listener, Query, Queries, Term, Expression, RecordData } from './types'
+import Store from '@orbit/store';
+import { getTermsOrExpression, getUpdatedRecords, shouldUpdate } from './helpers'
+import { Record, Transform, RecordOperation } from '@orbit/data'
+
+export class CacheManager extends Observable<Data> {
+
+  _store: Store
+
+  constructor (store: Store) {
+    super()
+    this._store = store
+  }
+
+  subscribe (id: string, listener: Listener<Data>) {
+    const unsubscribe = super.subscribe(id, listener)
+
+    if (Object.keys(this._subscriptions).length === 0) {
+      this._store.on('transform', this._compare)
+    }
+
+    return () => {
+      unsubscribe()
+
+      if (Object.keys(this._subscriptions).length === 0) {
+        this._store.off('transform', this._compare)
+      }
+    }
+  }
+
+  query (queryOrQueries: Query | Queries): Data {
+    const termsOrExpression = getTermsOrExpression(queryOrQueries)
+
+    return this._query(termsOrExpression)
+  }
+
+  _query (termsOrExpression: Term[] | Expression): Data {
+    let data: RecordData = null
+    let isError: boolean = false
+
+    try {
+      data = !Array.isArray(termsOrExpression)
+        ? this._store.cache.query(termsOrExpression) as Record
+        : termsOrExpression
+          .map(({ key, expression }) => ({ [key]: this._store.cache.query(expression) }))
+          .reduce((acc, record) => ({ ...acc, ...record }))
+
+    } catch {
+      isError = true
+    }
+
+    return [data, { isError, isLoading: false }]
+  }
+
+  _compare = (transform: Transform) => {
+    const { records, relatedRecords } = getUpdatedRecords(transform.operations as RecordOperation[])
+
+    Object.keys(this._subscriptions).forEach(id => {
+      const termsOrExpression = JSON.parse(id)
+
+      if (shouldUpdate(termsOrExpression, records, relatedRecords)) {
+        const data = this._query(termsOrExpression)
+        super.notify(id, data)
+      }
+    })
+  }
+}
--- src/QueryManager.ts
+++ src/QueryManager.ts
@@ -1,22 +1,20 @@
 import Store from '@orbit/store'
-import { Transform, RecordOperation, Record } from '@orbit/data'
 
-import { Observable } from './Observable'
-import { getUpdatedRecords, shouldUpdate, getTermsOrExpression, hashQueryIdentifier, validateOptions } from './helpers'
-import { Term, Queries, Expression, RecordData, Status, QueryRefs, Query, RecordObject, Options, SingleOptions, MultipleOptions, Data, Listener } from './types'
+import { getTermsOrExpression, hashQueryIdentifier, validateOptions } from './helpers'
+import { Queries, Query, Options, Data, Listener } from './types'
 import { FetchManager } from './FetchManager'
+import { CacheManager } from './CacheManager';
 
-export class QueryManager extends Observable<Data> {
-  _store: Store
+export class QueryManager {
+
   _fetchManager: FetchManager
+  _cacheManager: CacheManager
 
   constructor (store: Store) {
-    super()
-    this._store = store
     this._fetchManager = new FetchManager(store)
+    this._cacheManager = new CacheManager(store)
   }
 
-  // @ts-ignore
   subscribe (queryOrQueries: Query | Queries, listener: Listener<Data>, options?: Options) {
 
     const termsOrExpression = getTermsOrExpression(queryOrQueries)
@@ -25,20 +23,12 @@ export class QueryManager extends Observable<Data> {
 
     const id = hashQueryIdentifier(termsOrExpression, options)
 
-    if (Object.keys(this._subscriptions).length === 0) {
-      this._store.on('transform', this._compare)
-    }
-
-    const unsubscribe = super.subscribe(id, listener)
-    const unsubscribeToFetch = this._fetchManager.subscribe(id, listener)
+    const unsubscribeFromCache = this._cacheManager.subscribe(id, listener)
+    const unsubscribeFromFetch = this._fetchManager.subscribe(id, listener)
 
     return () => {
-      unsubscribe()
-      unsubscribeToFetch()
-
-      if (Object.keys(this._subscriptions).length === 0) {
-        this._store.off('transform', this._compare)
-      }
+      unsubscribeFromCache()
+      unsubscribeFromFetch()
     }
   }
 
@@ -46,42 +36,8 @@ export class QueryManager extends Observable<Data> {
     return this._fetchManager.query(queryOrQueries, options)
   }
 
-  queryCache (queryOrQueries: Query | Queries): [RecordData, Status] {
-    const termsOrExpression = getTermsOrExpression(queryOrQueries)
-
-    return this._queryCache(termsOrExpression)
+  queryCache (queryOrQueries: Query | Queries) {
+    return this._cacheManager.query(queryOrQueries)
   }
 
-  _queryCache (termsOrExpression: Term[] | Expression): [RecordData, Status] {
-    let data: RecordData = null
-    let isError: boolean = false
-
-    try {
-      data = !Array.isArray(termsOrExpression)
-        ? this._store.cache.query(termsOrExpression) as Record
-        : termsOrExpression
-          .map(({ key, expression }) => ({ [key]: this._store.cache.query(expression) }))
-          .reduce((acc, record) => ({ ...acc, ...record }))
-
-    } catch {
-      isError = true
-    }
-
-    return [data, { isError, isLoading: false }]
-  }
-
-  _compare = (transform: Transform) => {
-    const { records, relatedRecords } = getUpdatedRecords(transform.operations as RecordOperation[])
-
-    Object.keys(this._subscriptions).forEach(id => {
-      const termsOrExpression = JSON.parse(id)
-
-      const isLoading = this._fetchManager._queryRefs[id] ? this._fetchManager._queryRefs[id].isLoading : false
-
-      if (!isLoading && shouldUpdate(termsOrExpression, records, relatedRecords)) {
-        const data = this._queryCache(termsOrExpression)
-        super.notify(id, data)
-      }
-    })
-  }
 }
